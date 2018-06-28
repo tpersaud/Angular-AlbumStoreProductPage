dcfb2c62a47d0c7aa7ad036bb9329cb4238a1124
Terrence Persaud Jr <terrence.persaud@gmail.com>
2018-06-28 14:23:39 -0500

    added tracklist component

---
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+import { Album } from '../album';
+import { Track } from '../track';
+
+@Component({
+  selector: 'app-product-tracklisting',
+  templateUrl: './product-tracklisting.component.html',
+  styleUrls: ['./product-tracklisting.component.css']
+})
+export class ProductTracklistingComponent implements OnInit {
+  albumInfo:Album;
+
+  constructor(private _productServie:ProductService) { }
+
+  ngOnInit() {
+    this._productServie.getAlbum(1).subscribe(
+      (response) => this.albumInfo = response);
+  }
+
+}
